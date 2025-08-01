c10cbdaad9c90fe015fb75d876497ee3fbbfba92
kasarapuvignesh <vigneshnani519@gmail.com>
2025-08-01 15:44:31 +0530

    chat bot completed

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+
+app = Flask(__name__)
+
+
+load_dotenv()
+
+PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
+GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
+
+os.environ["PINECONE_API_KEY"]=PINECONE_API_KEY
+os.environ["GEMINI_API_KEY"]=GEMINI_API_KEY
+
+
+embeddings = download_embeddings()
+index_name = "medical-chatbot" 
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+
+
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+chat_model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.2,
+    google_api_key=GEMINI_API_KEY  # 👈 Add this
+)
+
+
+question_answer_chain = create_stuff_documents_chain(chat_model, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
